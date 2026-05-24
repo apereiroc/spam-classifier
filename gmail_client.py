684875cedd5fd16b53ee05e3fe10a7b12b86e444
Asier Pereiro <asier.pereiro.castro@cern.ch>
2026-05-24 13:08:20 +0200

    handle credentials and token paths as environment variables

--- gmail_client.py
+++ gmail_client.py
@@ -11,19 +11,21 @@ SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 # thank you gmail api docs
 def get_gmail_service():
     creds = None
+    token_path = os.getenv("GMAIL_TOKEN_PATH", "token.json")
+    credentials_path = os.getenv("GMAIL_CREDENTIALS_PATH", "credentials.json")
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open(token_path, "w") as token:
             token.write(creds.to_json())
     return build("gmail", "v1", credentials=creds)
