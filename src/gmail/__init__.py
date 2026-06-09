0c5da0080f06ca8f907e97415feb817ec220c3ef
Asier Pereiro <asier.pereiro.castro@cern.ch>
2026-06-09 23:41:31 +0200

    fix imports

--- src/gmail/__init__.py
+++ src/gmail/__init__.py
@@ -1,4 +1,4 @@
-from client import get_gmail_service
+from .client import get_gmail_service
 
 __all__ = [
     "get_gmail_service",
--- src/logging/__init__.py
+++ src/logging/__init__.py
@@ -1,4 +1,4 @@
-from logger import get_logger
+from .logger import get_logger
 
 __all__ = [
     "get_logger",
